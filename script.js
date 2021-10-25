a5abc95f7521df3bc082110a6a47858759217d65
dsharma22 <sharmadeepshika22@gmail.com>
2021-10-26 05:08:37 +1200

    changes to functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const cancelForm = document.getElementById("cancel-form");
 const saveBtn = document.getElementById("save");
 const webName = document.getElementById("website-name");
 const webUrl = document.getElementById("website-url");
+const bookmarkForm = document.querySelector(".bookmark-form");
 
 // Global Variables
 let removeBookmark;
@@ -13,18 +14,25 @@ let bookmarkObj = {};
 
 // Creating Bookmark Elements
 const createBookmark = (name, url) => {
+  // Bookmark
   let bookmark = document.createElement("div");
   bookmark.classList.add("bookmark");
+  // Title
   let title = document.createElement("div");
   title.classList.add("title");
-  let info = document.createElement("div");
-  info.classList.add("info");
+  // Close Icon
   let icon = document.createElement("i");
   icon.classList.add("fas", "fa-times", "remove");
   icon.setAttribute("title", "Delete Bookmark");
-  let link = document.createElement("a");
+  // Info --> favicon + link
+  let info = document.createElement("div");
+  info.classList.add("info");
+  // Favicon
   let favicon = document.createElement("img");
   favicon.setAttribute("src", `${url}/favicon.ico`);
+  favicon.setAttribute("alt", "favicon");
+  // Link
+  let link = document.createElement("a");
   link.setAttribute("href", url);
   link.setAttribute("target", "_blank");
   link.textContent = name;
@@ -72,30 +80,67 @@ const loadBookmarks = () => {
 addBtn.addEventListener("click", () => {
   container.style.position = "absolute";
   overlay.classList.add("show-overlay");
+  webName.focus();
 });
 
-// Cancel Form Event Listener
-cancelForm.addEventListener("click", () => {
+// Hiding Overlay
+const removeOverlay = () => {
   container.style.position = "static";
   overlay.classList.remove("show-overlay");
-});
+};
+
+// Cancel Form Event Listener
+cancelForm.addEventListener("click", removeOverlay);
 
 // Save Event Listener
-saveBtn.addEventListener("click", (e) => {
+bookmarkForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let name = webName.value;
   let url = webUrl.value;
-  bookmarkObj[name] = url;
 
-  createBookmark(name, url);
-  // Local Storage
-  localStorage.setItem("bookmarks", JSON.stringify(bookmarkObj));
-  // resetting input
-  webUrl.value = "";
-  webName.value = "";
-  container.style.position = "static";
-  overlay.classList.remove("show-overlay");
+  if (!url.includes("http://", "https://")) {
+    url = `https://${url}`;
+  }
+
+  if (!validateForm(name, url)) {
+    return false;
+  } else if (Object.keys(bookmarkObj).includes(name)) {
+    alert("Bookmark already exists!");
+  } else {
+    bookmarkObj[name] = url;
+    createBookmark(name, url);
+    // Local Storage
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarkObj));
+    // resetting input
+    webUrl.value = "";
+    webName.value = "";
+    container.style.position = "static";
+    overlay.classList.remove("show-overlay");
+  }
 });
+
+// Window event Listener
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("overlay")) {
+    removeOverlay();
+  }
+});
+
+const validateForm = (name, url) => {
+  const expression =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  let regex = new RegExp(expression);
+  if (!name || !url) {
+    alert("Please fill in appropriate inputs");
+    return false;
+  }
+  if (!url.match(regex)) {
+    alert("Please provide a valid URL");
+    return false;
+  }
+  // Valid
+  return true;
+};
 
 // OnLoad
 loadBookmarks();
